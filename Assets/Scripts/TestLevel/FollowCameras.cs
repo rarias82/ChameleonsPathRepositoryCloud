effbe88607a19d3966fa42135439f37c2f3b2ae6
Rodrigo <104698296+rarias82@users.noreply.github.com>
2022-08-30 20:37:10 -0400

    Nuevos cambios

--- Assets/Scripts/TestLevel/FollowCameras.cs
+++ Assets/Scripts/TestLevel/FollowCameras.cs
@@ -55,20 +55,6 @@ public class FollowCameras : MonoBehaviour
         transform.position = new Vector3(X + offset.x, trPlayer.position.y + offset.y, Z + offset.z);
     }
 
-    bool CheckXMargingX()
-    {
-        return (transform.position.x - trPlayer.position.x)< xMargin;
-    }
-
-    bool CheckXMargingZ()
-    {
-        return (transform.position.z - trPlayer.position.z) < zMargin;
-    }
-
-    bool CheckMarging()
-    {
-        return (transform.position.x > minXandZ.x && transform.position.x < maxXandZ.x);
-    }
     void Tracking()
     {
         float targetX = trPlayer.position.x;
@@ -77,16 +63,7 @@ public class FollowCameras : MonoBehaviour
         
         targetX = Mathf.Clamp(targetX, minXandZ.x, maxXandZ.x);
         targetZ = Mathf.Clamp(targetZ, minXandZ.z, maxXandZ.z);
-        //targetY = Mathf.Clamp(targetY, minXandY.y, maxXandY.y);
-
-        //if (CheckXMargingX() || CheckXMargingZ())
-        //{
-        //    targetX = Mathf.Lerp(transform.position.x, trPlayer.position.x, xSmooth * Time.deltaTime);
-        //    targetZ = Mathf.Lerp(transform.position.z, trPlayer.position.z, zSmooth * Time.deltaTime);
-        //    //transform.position = Vector3.Lerp(transform.position, trPlayer.position, xSmooth * Time.deltaTime);
-        //}
-
-        //transform.position = new Vector3(targetX, transform.position.y, targetZ);
+       
 
         transform.position = Vector3.Lerp(transform.position, new Vector3(posX,transform.position.y,posZ), xSmooth * Time.deltaTime);
     }
@@ -96,51 +73,29 @@ public class FollowCameras : MonoBehaviour
     {
         if (mode == Modo.InGame)
         {
-            //Vector3 destiny = trPlayer.position + offset;
-
-            //Vector3 newDestiny = new Vector3(destiny.x, destiny.y, destiny.z);
 
             posX = trPlayer.position.x;
             posZ = trPlayer.position.z;
             float X = Mathf.Clamp(posX, minXandZ.x, maxXandZ.x);
             float Z = Mathf.Clamp(posZ, minXandZ.z, maxXandZ.z);
 
-         
             
             transform.position = Vector3.Lerp(transform.position, new Vector3(X + offset.x, trPlayer.position.y + offset.y, Z + offset.z), xSmooth * Time.deltaTime);
             
 
-            //if (TposZ > minXandZ.z + offset.z && TposZ < maxXandZ.z)
-            //{
-            //    posZ = TposZ;
-            //}
-
 
 
             
 
         }
 
-        if (mode == Modo.InLimit)
-        {
-            //Vector3 destiny = trPlayer.position + offset;
-
-            //Vector3 newDestiny = new Vector3(destiny.x, destiny.y, destiny.z);
-
-            Tracking();
-        }
+      
 
         if (mode == Modo.InDialogue)
         {
-            //Vector3 destiny = trPlayer.position + dialogueOffset;
-
-            ////Vector3 newDestiny = new Vector3(destiny.x, destiny.y, destiny.z);
-
-            //transform.position = Vector3.Lerp(transform.position, destiny, smoothness * Time.deltaTime);
+            
         }
-        //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * sensivityMouse, Vector3.up) * offset;
-
-        //transform.LookAt(trPlayer);
+       
 
         MyCameras.orthographicSize = Mathf.Clamp(MyCameras.orthographicSize, 3.5f, 7.5f); // Límites
 
